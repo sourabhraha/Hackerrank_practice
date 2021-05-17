f742bd28b9ac1270ec40cfae7c34729dac8b3688
sourabh <sourabhrahafbg@gmail.com>
2021-05-18 01:50:54 +0530

    updated

---
+++ q2.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+
+    ll test_cases;
+    cin>>test_cases;
+
+    while (test_cases--)
+    {
+        ll n;
+        cin>>n;
+
+        vector<string>v;
+
+        for(ll i=0; i<n; i++)
+        {
+            string str;
+            cin>>str;
+            v.push_back(str);
+        }
+
+        
+
+
+
+    }
+    
+
+
+
+
+
+
+
+
+    return 0;
+}
